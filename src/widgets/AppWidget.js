bb88a9aeb3484e7ff05ec5bafc64b5f86a8c2df3
Dmitry <main.mailbox.vda@gmail.com>
2018-12-17 12:18:45 +0300

    Added template widget

--- src/widgets/AppWidget.js
+++ src/widgets/AppWidget.js
@@ -1,33 +1,67 @@
 import React from "react";
-import { connect } from "react-redux";
-import { Text, View } from "react-native";
-import { Button } from "react-native-elements";
+import { Text, View, Image } from "react-native";
+import { setExpandable } from "react-native-today-widget";
+import style from "./style";
 
 class AppWidget extends React.Component {
   render() {
+    const isExpandable = true;
+    const maxHeight = 200;
+
+    setExpandable(isExpandable, maxHeight);
+
+    const onLayout = event => {
+      const height = event.nativeEvent.layout.height;
+      if (height <= 110) {
+        console.log("widget is in compact mode");
+      } else if (height > 110) {
+        console.log("widget is in expanded mode");
+      }
+    };
     return (
       <View
+        onLayout={onLayout}
         style={{
           flex: 1,
-          backgroundColor: "blue",
-          justifyContent: "center",
-          alignItems: "center"
+          flexDirection: "row",
+          justifyContent: "space-between"
         }}
       >
-        <Text style={{ color: "#fff", fontSize: 24 }}>
-          {this.props.activeList}
-        </Text>
+        <View
+          style={{
+            backgroundColor: "green",
+            width: "33.333%",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <Text style={{ fontSize: 16, marginBottom: 5 }}>column_1</Text>
+        </View>
 
+        <View
+          style={{
+            backgroundColor: "red",
+            width: "33.333%",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <Text style={{ fontSize: 16, marginBottom: 5 }}>column_2</Text>
+        </View>
+
+        <View
+          style={{
+            backgroundColor: "yellow",
+            width: "33.333%",
+            flexDirection: "column",
+            alignItems: "center"
+          }}
+        >
+          <Text style={{ fontSize: 16, marginBottom: 5 }}>column_3</Text>
+        </View>
       </View>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  activeList: state.main.activeList
-});
-
-export default connect(
-  mapStateToProps,
-  {}
-)(AppWidget);
+export default AppWidget;
